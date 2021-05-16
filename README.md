5bafdc36e7520410e82175680bd39f7c5e411554
Eleomar Dorneles <eleomar.dorneles.18@outlook.com>
2021-05-16 11:43:30 -0400

    Stylizada

--- README.md
+++
@@ -1,7 +0,0 @@
-# flappy-bird
-Apenas um jogo para testar os conhecimentos em programação
-
-
-Acesse pelo Link: 
-
-https://oeleomar.github.io/flappy-bird/
---
+++ script.js
@@ -0,0 +1,6 @@
+function returnTo(){
+    window.location.replace("/flappy-bird/flappy-bird/index2.html")
+}
+function goFlappy(){
+    window.location.replace("/flappy-bird/flappy-bird/index2.html")
+}
